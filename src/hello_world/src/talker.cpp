2ccc9b31c4100bbf8aa10cabdc8efed7d57a9c76
Gagan Deepak R <gagan.databee@gmail.com>
2022-06-27 16:58:46 +0530

    commented file

--- src/hello_world/src/talker.cpp
+++ src/hello_world/src/talker.cpp
@@ -10,20 +10,20 @@ int main(int argc, char** argv){
     //create a topic publisher with the help of node handler
     ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter",1000);//chatter is topic name , chatter_pub is handler
     //create a sleep handler
-    ros::Rate loop_rate(10); //sleep for 10 milliseconds
+    ros::Rate loop_rate(1); //its the frequency of execution
     int count = 0; //temp variable to count
 
     while(ros::ok()) {
-        //create a string variable to store the message
+        //create a string variable to publish the message
         std_msgs::String msg;
-        //create a variable to put it to terminal like cout ; here we are using stringstream object ss;
+        //create a variable to put it to terminal like cout ; here we are using stringstream object ss; like temp variable
         std::stringstream ss;
-        ss<<"Hello World "<<count;
-        msg.data = ss.str();
+        ss<<"Hello World "<<count; //put the data into ss
+        msg.data = ss.str(); //now transfer to message variable
         ROS_INFO("%s",msg.data.c_str());
         chatter_pub.publish(msg);
-        ros::spinOnce();
-        loop_rate.sleep();
+        ros::spinOnce(); //need to use after publishing the topic
+        loop_rate.sleep(); 
         ++count; //increament the count variable
     }
     return 0;
