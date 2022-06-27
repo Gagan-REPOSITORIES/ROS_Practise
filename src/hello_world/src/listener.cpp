e6a7ec7ca25b55b92b7e311d833a53d4934d1387
Gagan Deepak R <gagan.databee@gmail.com>
2022-06-27 18:09:52 +0530

    created listener file

---
+++ src/hello_world/src/listener.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+void callBack(const std_msgs::String::ConstPtr& msg){
+    //display the received message
+    ROS_INFO("Listener [%s]", msg->data.c_str());
+}
+
+int main(int argc, char** argv){
+    //initialize the node
+    ros::init(argc, argv, "hello_world_listener");
+    //create the node handler
+    ros::NodeHandler nh;
+    //create a subscriber handler
+    ros::Subscriber sub = nh.subcribe("chatter",1000,callBack);
+    ros::spin();
+    return 0;
+}
